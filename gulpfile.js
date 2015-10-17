1898f58ff20c4b8447ff575f20859f0dcfa89146
Sercan <sercan.altundas@gmail.com>
2015-10-18 00:19:23 +0300

    Extending gulpfile and fixing earlier css only changes.
    Gulp file is extended to watch the partials folder.
    
    Some css only fixes that I made was gone as they were not in scss files,
    those are fixed and compiled using gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp'),
 sass = require('gulp-sass')
 
 gulp.task('sass', function(){
-	gulp.src('./assets/css/main.scss')
+	gulp.src(['./assets/css/main.scss', './assets/css/partials/*.scss'])
 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(gulp.dest('./assets/css'))
 	})
 
 gulp.task('default', ['sass'], function(){
-	gulp.watch('./assets/css/main.scss', ['sass'])
+	gulp.watch(['./assets/css/main.scss', './assets/css/partials/*.scss'], ['sass']);
 	})
