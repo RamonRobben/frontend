2960a681bed5df8388be79bc9e773d79195ea8a1
Andrew Sampson <andrew@codeusa523.org>
2015-10-18 17:58:02 -0400

    Revert "Fix CORS when use "createMediaElementSource" and minor changes"

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,12 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    sass = require('gulp-sass'),
-    reload = browserSync.reload;
+sass = require('gulp-sass')
 
-gulp.task('sass', function() {
-  gulp.src(['./assets/css/main.scss', './assets/css/partials/*.scss'])
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
-});
+gulp.task('sass', function(){
+	gulp.src(['./assets/css/main.scss', './assets/css/partials/*.scss'])
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('./assets/css'))
+	})
 
-gulp.task('serve', function() {
-  browserSync({
-    notify: false,
-    server: {
-      baseDir: '.',
-      middleware: function (request, response, next) {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      }
-    }
-  });
-
-  gulp.watch(['./assets/css/main.scss', './assets/css/partials/*.scss'], ['sass', reload]);
-})
-
-gulp.task('default', ['serve']);
+gulp.task('default', ['sass'], function(){
+	gulp.watch(['./assets/css/main.scss', './assets/css/partials/*.scss'], ['sass']);
+	})
